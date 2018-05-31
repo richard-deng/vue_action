65940e3f691a5b80b1d91b55215083efd426a704
邓成 <dengcheng@qfpay.com>
2018-05-31 16:43:51 +0800

    修改

---
+++ libs/util.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+// 基本配置
+const Util  = {
+    base_path: 'http://127.0.0.1:8084'
+};
+
+// Ajax配置
+Util.ajax = axios.create({
+    baseURL: Util.base_path
+});
+// 添加响应拦截器
+
+Util.ajax.interceptors.response.use(res => {
+    return res.data;
+});
+
+export default Util;
+
--- views/table.vue
+++ views/table.vue
@@ -48,10 +48,20 @@
                 :visible.sync="dialogVisible"
                 width="30%"
                 :before-close="handleClose">
-            <span>这是一段信息</span>
+            <el-form :model="form">
+                <el-form-item label="活动名称" :label-width="formLabelWidth">
+                    <el-input v-model="form.name" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="活动区域" :label-width="formLabelWidth">
+                    <el-select v-model="form.region" placeholder="请选择活动区域">
+                        <el-option label="区域一" value="shanghai"></el-option>
+                        <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+            </el-form>
             <span slot="footer" class="dialog-footer">
-            <el-button @click="dialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
             </span>
         </el-dialog>
     </div>
@@ -67,7 +77,8 @@
                 currentPage: 1,
                 total: 0,
                 page_size: 10,
-                dialogVisible: false
+                dialogVisible: false,
+                formLabelWidth: '150px',
             }
         },
         methods: {
